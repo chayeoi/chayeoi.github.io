4f00dd9a55d0068a25ef5bcc20df9250d1fa5a0a
chayeoi <y2oniary@gmail.com>
2018-01-14 14:10:07 +0900

    enable nav to toggle

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -3,8 +3,22 @@ $(document).ready(function() {
   // Menu Settings
   $('.menu-icon, .menu-icon-close').click(function(e) {
     e.preventDefault();
+    e.stopPropagation();
+    
     $('.flex-container').toggleClass('active');
+    $('.nav-sub-lists').removeClass('nav-sub-lists--active');
   });
+
+  $(document).click(function(e) {
+    if ($('.flex-container').hasClass('active')) {
+      $('.flex-container').removeClass('active');
+    }
+  })
+
+  // Sub Menu Settings
+  $('.nav-sup-title').click(function(e) {
+    $(this).siblings('.nav-sub-lists').toggleClass('nav-sub-lists--active');
+  })
 
   // Search Settings
   $('.search-icon').on('click', function(e){
